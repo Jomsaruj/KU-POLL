340e41957c5f573665977704ccf4ab493f49dd54
saruj <jomsaruj@sarujs-MacBook-Pro.local>
2020-09-17 20:50:04 +0700

    Add expired date

--- polls/admin.py
+++ polls/admin.py
@@ -15,10 +15,11 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date','end_date'], 'classes': ['collapse']}),
+        
     ]
     inlines = [ChoiceInline]
-    list_display  = ('question_text', 'pub_date', 'was_published_recently')
+    list_display  = ('question_text', 'pub_date', 'end_date', 'was_published_recently', )
     
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+#admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -7,13 +7,33 @@ __author__      = "Saruj Sattayanurak"
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField("Date published")
+    end_date = models.DateTimeField("Date expired")
     
     def __str__(self):
         return self.question_text
-    
+        
+    """
+    Return True only if pub_date is in the past.
+    """
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days = 1) <= self.pub_date <= now
+        
+    """
+    Returns True if current date is on or after question’s publication date
+    """
+    def is_published(self):
+        now = timezone.now()
+        return now >= self.pub_date
+        
+    """
+    Returns True if voting is currently allowed for this question
+    """
+    def can_vote(self):
+        now = timezone.now()
+        return self.is_published and now <= self.end_date
+ 
+        
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
